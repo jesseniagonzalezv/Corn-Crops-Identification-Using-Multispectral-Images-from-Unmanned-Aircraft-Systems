003944b36c61aaaa1e90df531aa8008d4f3ce311
jesseniagonzalezv <jessenia.gonzalezv@pucp.edu.pe>
2021-01-09 15:07:40 -0500

    test_again_channel_without_predictions

--- plot_prediction.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ bash
-#Run as bash plot_prediction.sh
-
-echo ploting
-
-#python plotting.py --out-file 'HR' --stage 'test'  --name-model 'UNet' --count 277
-
-python plotting.py --out-file 'HR' --stage 'test' --name-file '_100_percent' --name-model 'UNet' --count 277 
